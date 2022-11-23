10c90684ff7fbcdc9011d715302095e2be482183
Nazar <santanazar123@icloud.com>
2022-11-23 19:03:34 +0200

    bullshit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+function bigHack (name, age){
+    let a = 2, 
+        b = 2
+    if(a || b === 1) {
+        console.log('1')
+    }else if(a || b === 2){
+        console.log('2')
+    }else{
+        console.log('3')
+    }
+
+    for(let i = 1; i <= 10; i++){
+        console.log(i)
+    }
+
+    let i = 1; 
+
+    while(i < 10){
+        i++;
+        console.log(i)
+    };
+     
+    };
+
+bigHack('Nazar', 18);
+
+
+
+let helloFriend = [],
+goodBayFriend = [1, 2, 3, 4, 5]
+
+if(goodBayFriend[0] === 1){
+console.log('4')
+}else{
+console.log('2')
+};
+
+let a = 19; 
+
+if(a < 18){
+    console.log('you can!')
+}else if(a > 18){
+    console.log('sorry, u old')
+}else{
+    console.log('sorry, u very young')
+}; 
+
+
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+};
+
+
+
+
+
+
+
